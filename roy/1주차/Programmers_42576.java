0272df7599052847c44cc48b50e1d1a63d0cddaa
goyounha11 <goyounha11@naver.com>
2023-12-14 14:13:32 +0900

    완주하지 못한 사람 찾기

--- roy/1주차/Programmers_42576.java
+++ roy/1주차/Programmers_42576.java
@@ -1,6 +1,25 @@
 
 public class programmers_42576 {
   public static void main(String[] args) {
+    String answer = "";
 
+    String[] participant = {"leo", "kiki", "eden"};
+    String[] completion = {"eden", "kiki"};
+
+    Map<String, Integer> completionCount = new HashMap<>();
+
+    for (String finisher : completion) {
+      completionCount.put(finisher, completionCount.getOrDefault(finisher, 0) + 1);
+    }
+
+    for (String runner : participant) {
+      if (!completionCount.containsKey(runner) || completionCount.get(runner) == 0) {
+        answer = runner;
+        break;
+      }
+      completionCount.put(runner, completionCount.get(runner) - 1);
+    }
+
+    System.out.println(answer);
   }
 }
